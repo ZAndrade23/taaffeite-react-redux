6e91892bac8f1c95d11435675bdddd7326a97d16
Zach Andrade <zach_andrade@yahoo.com>
2023-11-08 13:03:23 -0600

    In classs we did redux today putting a counter with increase n decrease buttons as well as a type field

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,47 @@
 import BookForm from '../BookForm/BookForm';
 import BookList from '../BookList/BookList';
-
+import ListName from '../ListName/ListName';
+// use useSelector to pull info out th Redux store
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+  // Connect a local variable to the reducer
+const count = useSelector(store => store.count);
+const listName = useSelector(store => store.listName);
+// dispatch is used to send data to Redux
+  const dispatch = useDispatch();
 
-  // TODO - GET Book List from server
+const increaseCount = () => {
+  // send data to redux using dispatch
+  const action = { type: 'INCREASE' };
+  dispatch(action)
+}
+const decreaseCount = () => {
+  const action = { type: 'DECREASE'};
+  dispatch(action)
+}
+
+const resetCount = () => {
+  const action = { type: 'RESET'};
+  dispatch(action)
+}
+// TODO - GET Book List from server
 
   return (
     <div className="App">
-      <header><h1>Books w/ Redux!</h1></header>
+      <header><h1>{listName}</h1></header>
+      <div>
+        <button onClick={increaseCount}
+        >Increase</button>
+        <button onClick={decreaseCount}
+        >Decrease</button>
+        <button onClick={resetCount}
+        >Reset</button>
+      </div>
+      <div>Count: {count} </div>
+      <h4>Name Your Book List</h4>
+      <ListName/>
       <main>
         <BookForm />
         <BookList />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// step 1: import Provider, applyMIddleware, combineReducers, createstore
+
 import { Provider } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
+// End Step 1
+
+
 import App from './components/App/App';
 import './index.css';
+
+// Create a reducer to store a counter
+// const [count, setCount] = useState();
+//    name     value = initial value      action
+const count = (state= 0,                  action) => {
+ if (action.type === 'INCREASE') {
+  return state + 1; // ! NOT state += 1
+ }else if (action.type === 'DECREASE') {
+  return state -1;
+ } else if (action.type === 'RESET') {
+  return  0;
+ }
+ // Returning state means the reducer is unchanged
+  return state;
+}
+
+const listName = (state = 'title', action) => {
+  if (action.type === 'SET_LIST_NAME') {
+    //passing new list name via action.payload
+    return action.payload;
+  }
+  // ! Reducers must always return state by default
+return state;
+}
+
 
 const bookList = (state = [], action) => {
   // TODO - set book list with data from server
@@ -13,7 +44,10 @@ const bookList = (state = [], action) => {
 
 const reduxStore = createStore(
   combineReducers({
-    bookList
+   // Step 2: Only part of this block of code needed to be changed
+    bookList,
+    count,
+    listName
   }),
   applyMiddleware(logger)
 );
@@ -21,6 +55,7 @@ const reduxStore = createStore(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* Step 3: Wrap the App in a Provider */}
     <Provider store={reduxStore}>
       <App />
     </Provider>
